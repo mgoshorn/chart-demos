c8cc7189426c3ca87cb646c140a70da58f916d1d
Mitchell Goshorn <msgoshorn@gmail.com>
2017-12-16 13:55:15 -0500

    Bar chart example added

--- src/app/components/bar/bar.component.ts
+++ src/app/components/bar/bar.component.ts
@@ -9,7 +9,24 @@ export class BarComponent implements OnInit {
 
   constructor() { }
 
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    response: true
+  };
+
+  // Chart data - In practice this should all be loaded from a service making API calls
+  public barChartLabels: string[] = ['Project', 'Exam', 'Other', 'Verbal'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+
+  public barChartData: any[] = [
+    {data: [97.5, 73.18, 26, 85.2], label: 'Trainee'},
+    {data: [90.16, 72.18, 40.88, 82], label: 'Batch'}
+  ];
+
   ngOnInit() {
   }
+
+
 
 }
